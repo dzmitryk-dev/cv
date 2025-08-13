df50fc5c2d8d1ede9aa6e5bb95201afb40603c02
Dzmitry_Kalianchuk <dzmitry_kalianchuk@epam.com>
2025-08-13 12:13:27 +0200

    Upgrade gradle

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,17 +20,6 @@ kotlin {
         }
     }
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-
     jvm()
 
     @OptIn(ExperimentalWasmDsl::class)
